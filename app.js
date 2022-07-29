f7bda5c2a21f575b8e38260e80ee041d2ce05682
Brenner Silva <95314570+br3nerm@users.noreply.github.com>
2022-07-29 14:31:43 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,51 @@
+
+let btn = document.querySelector('#button-menu');
+let menuList = document.querySelector('#menu');
+
+
+
+
+btn.addEventListener('click', function () {
+
+    
+
+    if (menuList.style.display === 'block') {
+
+        menuList.style.display = 'none';
+    }
+
+    else {
+        menuList.style.display = 'block'
+    }
+
+
+});
+
+
+function clique() {
+
+    document.querySelector("#info").innerHTML = 'Minhas esperiências com HTML5<br>são de 2 meses e breve vou estar<br> mostrando mais projetos';
+
+}
+
+function cliqueDois() {
+
+    document.querySelector("#info").innerHTML = 'Minhas esperiências com CSS<br>são de 2 meses e breve vou estar<br> mostrando mais projetos';
+
+}
+
+
+function cliqueTres() {
+
+    document.querySelector("#info").innerHTML = 'Minhas esperiências com JS<br>é de 1 mês e breve vou estar<br> mostrando mais projetos';
+
+}
+
+
+
+
+
+
+
+
+
