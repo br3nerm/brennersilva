db2f4030799de007b8af622151268a32a89fe485
Brenner Silva <95314570+br3nerm@users.noreply.github.com>
2022-12-08 21:02:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,20 +24,20 @@ btn.addEventListener('click', function () {
 
 function clique() {
 
-    document.querySelector("#info").innerHTML = 'Minhas esperiências com HTML5<br>são de 2 meses e breve vou estar<br> mostrando mais projetos';
+    document.querySelector("#info").innerHTML = 'Minhas esperiências com HTML5<br>são de 10 meses e breve vou estar<br> mostrando mais projetos';
 
 }
 
 function cliqueDois() {
 
-    document.querySelector("#info").innerHTML = 'Minhas esperiências com CSS<br>são de 2 meses e breve vou estar<br> mostrando mais projetos';
+    document.querySelector("#info").innerHTML = 'Minhas esperiências com CSS<br>são de 8 meses e breve vou estar<br> mostrando mais projetos';
 
 }
 
 
 function cliqueTres() {
 
-    document.querySelector("#info").innerHTML = 'Minhas esperiências com JS<br>é de 1 mês e breve vou estar<br> mostrando mais projetos';
+    document.querySelector("#info").innerHTML = 'Minhas esperiências com JS<br>é de 6 mês e breve vou estar<br> mostrando mais projetos';
 
 }
 
